70af16f7f1f0175d7625bea0d200c3abfe9f773e
Scott King <jsking@andrew.cmu.edu>
2022-11-09 18:26:54 -0500

    Update build script

--- build.bat
+++ build.bat
@@ -1,17 +1,17 @@
 :: Scott King (10/31/22)
 :: Runs blender asset pipeline and builds game.exe
-:: All references to blender need to be swapped to Blender 3.2 for this to work on my laptop
+:: All references to blender need to be swapped to Blender 3.2 for this to work on my laptop or 2.93 to work on my desktop
 @ECHO OFF
 ECHO Running pipeline and building game.exe
 ECHO ==============================================
 Echo Exporting meshes...
-"C:\Program Files\Blender Foundation\Blender 2.93\blender.exe" --background --python scenes/export-meshes.py -- scenes/proto-world.blend dist/proto-world.pnct
+"C:\Program Files\Blender Foundation\Blender 3.2\blender.exe" --background --python scenes/export-meshes.py -- scenes/proto-world2.blend dist/proto-world2.pnct
 ECHO ==============================================
 Echo Exporting walkmesh...
-"C:\Program Files\Blender Foundation\Blender 2.93\blender.exe" --background --python scenes/export-walkmeshes.py -- scenes/proto-world.blend:WalkMeshes dist/proto-world.w
+"C:\Program Files\Blender Foundation\Blender 3.2\blender.exe" --background --python scenes/export-walkmeshes.py -- scenes/proto-world2.blend:WalkMeshes dist/proto-world2.w
 ECHO ==============================================
 Echo Exporting scene...
-"C:\Program Files\Blender Foundation\Blender 2.93\blender.exe" --background --python scenes/export-scene.py -- scenes/proto-world.blend dist/proto-world.scene
+"C:\Program Files\Blender Foundation\Blender 3.2\blender.exe" --background --python scenes/export-scene.py -- scenes/proto-world2.blend dist/proto-world2.scene
 ECHO ==============================================
 Echo Running maekfile...
 node Maekfile.js -j1 &
