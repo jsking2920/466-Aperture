c2728f8d14af3bc1cf9fc57e36b7387c3e030339
Scott King <jsking@andrew.cmu.edu>
2022-11-09 20:17:54 -0500

    oops, pushed the wrong thing

--- Picture.hpp
+++ Picture.hpp
@@ -11,31 +11,31 @@ struct ScoreElement {
     uint32_t value = 1;
 };
 
+struct CreatureInfo {
 
+    Creature* creature;
+    float frag_count;
+    glm::vec3 player_to_creature;
+    std::vector<bool> are_focal_points_in_frame;
+};
 
+//data about the picture that is used for scoring
+struct PictureInfo { 
+    
+    CreatureInfo creatureInfo;
 
+    glm::vec2 dimensions;
+    std::shared_ptr<std::vector<GLfloat>> data;
+
+    glm::vec3 angle;
+    uint32_t total_frag_count;
+    std::list< std::pair<Scene::Drawable&, GLuint > > frag_counts;
+    std::list< CreatureInfo > creatures_in_frame;
+};
 
 struct Picture {
     
-    //data about the picture that is used for scoring
-    struct PictureInfo {
-
-        struct CreatureInfo {
-
-            Creature* creature;
-            float frag_count;
-            glm::vec3 player_to_creature;
-            std::vector<bool> are_focal_points_in_frame;
-        };
-
-        glm::vec2 dimensions;
-        std::shared_ptr<std::vector<GLfloat>> data;
-
-        glm::vec3 angle;
-        uint32_t total_frag_count;
-        std::list< std::pair<Scene::Drawable&, GLuint > > frag_counts;
-        std::list< CreatureInfo > creatures_in_frame;
-    };
+    PictureInfo pictureInfo;
 
     Picture() = default;
     explicit Picture(PictureInfo &stats);
