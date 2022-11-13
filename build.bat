95e93fcaeaa5982317e5d399425f15d8e970165d
Scott King <jsking@andrew.cmu.edu>
2022-11-13 01:05:52 -0500

    Merge in failed python script

--- build.bat
+++ build.bat
@@ -1,6 +1,11 @@
 :: Scott King (10/31/22)
 :: Runs blender asset pipeline and builds game.exe
+
+:: Notes
 :: All references to blender need to be swapped to Blender 3.2 for this to work on my laptop or 2.93 to work on my desktop
+:: to be run from path/to/466-Aperture/ 
+:: in the x64 Native Tools Command Prompt for VS 2022
+
 @ECHO OFF
 ECHO Running pipeline and building game.exe
 ECHO ==============================================
@@ -18,3 +23,9 @@ node Maekfile.js -j1 &
 ECHO ==============================================
 Echo All done! Starting game
 .\dist\game.exe
+
+:: Some other possible commands
+:: Non-woring script for cleaning up transparent pixels in textures: python3 ./scenes/clean-textures.py ./dist/assets/textures/
+:: Must have SDL2.dll in same folder with these .exe's for them to the run
+:: .\dist\show-meshes.exe .\dist\assets\proto-world2.pnct
+:: .\dist\show-scene.exe .\dist\assets\proto-world2.scene .\dist\assets\proto-world2.pnct
